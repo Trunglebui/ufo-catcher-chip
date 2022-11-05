1a92cb18e57ae470bcd80a53f65e64500fd3b551
Trunglebui <101624601+Trunglebui@users.noreply.github.com>
2022-11-05 17:13:49 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+led.enable(false)
+let X = 180
+let Y = 100
+ModuleWorld_PWM.Servo(ModuleWorld_PWM.mwServoNum.P1, X)
+ModuleWorld_PWM.Servo(ModuleWorld_PWM.mwServoNum.P2, Y)
+basic.forever(function () {
+    ModuleWorld_PWM.Servo(ModuleWorld_PWM.mwServoNum.P1, X)
+    ModuleWorld_PWM.Servo(ModuleWorld_PWM.mwServoNum.P2, Y)
+    if (ModuleWorld_Analog.Rocker(ModuleWorld_Analog.mwAnalogNum.AP3P4, ModuleWorld_Analog.enRocker.Up)) {
+        Y = Y + 1
+    } else if (ModuleWorld_Analog.Rocker(ModuleWorld_Analog.mwAnalogNum.AP3P4, ModuleWorld_Analog.enRocker.Down)) {
+        Y = Y - 1
+    } else if (ModuleWorld_Analog.Rocker(ModuleWorld_Analog.mwAnalogNum.AP3P4, ModuleWorld_Analog.enRocker.Left)) {
+        X = X - 1
+    } else if (ModuleWorld_Analog.Rocker(ModuleWorld_Analog.mwAnalogNum.AP3P4, ModuleWorld_Analog.enRocker.Right)) {
+        X = X + 1
+    }
+})
+basic.forever(function () {
+    if (X < 150) {
+        X = 150
+    } else if (X < 207) {
+        X = 207
+    } else if (Y < 70) {
+        Y = 70
+    } else if (Y > 140) {
+        Y = 140
+    }
+})
